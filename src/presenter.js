674b4baf0beb9ddeab75db9aaa822a4d74f2fe69
Xplay518 <gustavo.valencia@ucb.edu.bo.com>
2024-02-15 01:34:27 -0400

    Multiplicador

--- src/presenter.js
+++ src/presenter.js
@@ -1,3 +1,21 @@
+// presenter.js
+import multiplicar from "./sumador";
+
+const first = document.querySelector("#primer-numero");
+const second = document.querySelector("#segundo-numero");
+const form = document.querySelector("#sumar-form");
+const div = document.querySelector("#resultado-div");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+});
+
+/*
 import sumar from "./sumador";
 
 const first = document.querySelector("#primer-numero");
@@ -13,3 +31,4 @@ form.addEventListener("submit", (event) => {
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
 });
+*/
--- src/sumador.js
+++ src/sumador.js
@@ -1,5 +1,16 @@
+// sumador.js
+function multiplicar(a, b) {
+  return a * b;
+}
+
+export default multiplicar;
+
+//export default multiplicar;
+
+/*
 function sumar(a, b) {
   return a + b;
 }
 
 export default sumar;
+*/
